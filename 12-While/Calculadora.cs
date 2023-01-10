18ea6224785083e9f9f32521f59e506419f2fb86
Ana Clara campanati <campanatianaclara@gmail.com>
2023-01-10 18:36:19 -0300

    Adicionar arquivos de projeto.

---
+++ 12-While/Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_While
+{
+    internal class Calculadora
+    {
+
+        public static int Somar(int primeiroNumero, int segundoNumero)
+        {
+            int Somar = primeiroNumero + segundoNumero;
+            return Somar;
+        }
+        public static int Diminuir (int primeiroNumero, int segundoNumero)
+        {
+            int Diminuir = primeiroNumero - segundoNumero;
+            return Diminuir;
+        }
+        public static int Multiplicar (int primeiroNumero, int segundoNumero)
+        {
+            int Multiplicar = primeiroNumero * segundoNumero;
+            return Multiplicar;
+        }
+        public static int Dividir (int primeiroNumero, int segundoNumero)
+        {
+            int Dividir = primeiroNumero / segundoNumero;
+            return Dividir;
+        }
+        
+    }
+}
---
+++ 12-While/Program.cs
@@ -0,0 +1,48 @@
+﻿// Crie um algoritmo que receba números inteiros e enha como operador as opções + , - , * , / . (Método Soma, Método Subtração, Método Multiplicação, Método Divisão.
+
+using _12_While;
+
+Console.WriteLine("Deseja continuar operando? Digite S para SIM e N para NÃO");
+var entrada = Console.ReadLine();
+int total = 0;
+
+Console.WriteLine("Informe um número:");
+int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe um segundo número:");
+int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+while (entrada == "S")
+{
+    Console.WriteLine("Digite o operador:");
+    entrada = Console.ReadLine();
+
+    if (entrada == "+") 
+    {
+        total = Calculadora.Somar(primeiroNumero, segundoNumero);
+    }
+
+   if (entrada == "-")
+    {
+        total = Calculadora.Diminuir(primeiroNumero, segundoNumero);
+    }
+
+   if (entrada == "*")
+    {
+        total = Calculadora.Multiplicar(primeiroNumero, segundoNumero);
+    }
+
+   if (entrada == "/")
+    {
+        total = Calculadora.Dividir(primeiroNumero, segundoNumero);
+    }
+
+    Console.WriteLine("O total é: " + total);
+
+    Console.WriteLine("Deseja contiuar operando? Digite S para SIM e N para NÃõ.");
+    Console.ReadLine();
+}
+
+
+
+
